d5b110630e9c2f47e63d7631116d58ae7416ccc7
Marek Kochanowicz <sirherrbatka@gmail.com>
2023-03-29 19:13:06 +0200

    deleted separate thread in with-flownet

--- source/protocol/macros.lisp
+++ source/protocol/macros.lisp
@@ -1,19 +1,13 @@
 (cl:in-package #:giddy.protocol)
 
 
-(defmacro with-flownet ((flownet-form)
-                        &body body)
-  (with-gensyms (!read-thread !flownet)
-    `(let* ((*flownet* ,flownet-form)
-            (,!flownet *flownet*)
-            (,!read-thread
-              (bt:make-thread (lambda ()
-                                (iterate
-                                  (with lock = (bt:make-lock))
-                                  (until (bt:with-lock-held ((lock ,!flownet))
-                                           (endp (active-cells))))
-                                  (bt:condition-wait (condition-variable ,!flownet) lock))))))
-       (unwind-protect
-            ,@body
-         (bt:join-thread ,!read-thread))
-       *flownet*)))
+(defmacro execute-flownet ((flownet-form)
+                           &body body)
+  `(let ((*flownet* ,flownet-form))
+     ,@body
+     (iterate
+       (with lock = (bt:make-lock))
+       (until (bt:with-lock-held ((lock *flownet*))
+                (endp (active-cells))))
+       (bt:condition-wait (condition-variable *flownet*) lock))
+     *flownet*))
